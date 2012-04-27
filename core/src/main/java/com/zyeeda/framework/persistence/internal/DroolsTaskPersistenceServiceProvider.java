22c7e9a10120558cddea7148b55992c8b4224fd8
chenhaifeng <chenhaifeng@zyeeda.com>
2012-04-27 18:38:54 +0800

    move csg codes
    hg: 请输入提交日志。以 'HG:' 开始的行会被删除。

--- core/src/main/java/com/zyeeda/framework/persistence/internal/DroolsTaskPersistenceServiceProvider.java
+++
@@ -1,7 +0,0 @@
-package com.zyeeda.framework.persistence.internal;
-
-import com.zyeeda.framework.persistence.PersistenceService;
-
-public class DroolsTaskPersistenceServiceProvider extends AbstractPersistenceServiceProvider
-		implements PersistenceService {
-}
