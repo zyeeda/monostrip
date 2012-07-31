5f8d08b8a33d8f15ab4a269a52b7f4b93a353908
Shusen Gu <gushusen@zyeeda.com>
2012-07-31 17:05:20 +0800

    去掉重载接口

--- core/src/main/java/com/zyeeda/framework/bpm/TaskService.java
+++ core/src/main/java/com/zyeeda/framework/bpm/TaskService.java
@@ -1,19 +1,7 @@
 package com.zyeeda.framework.bpm;
 import java.util.Map;
 
-import org.jbpm.task.Task;
-
 public interface TaskService {
-    // 任务流转类型
-    public final static String TRANSITION_TYPE = "transitionType";
-    // 默认审批类型
-    public final static int TRANSITION_TYPE_DEFAULT = 1;
-    // 动态审批(顺序)
-    public final static int TRANSITION_TYPE_SEQUENTIAL = 2;
-    // 动态审批(并行)
-    public final static int TRANSITION_TYPE_PARALLEL = 3;
-    // 动态审批(并行子流程)
-    public final static int TRANSITION_TYPE_SUBPROCESS = 4;
     /**
      * 领取任务
      */
@@ -25,24 +13,11 @@ public interface TaskService {
      */
     public void complete(Long taskId, String comment, Map<String,Object> results);
     /**
-     * 根据任务实例提交任务
-     * 
-     * @param task
-     */
-    public void complete(Task task, String comment, Map<String,Object> results);
-
-    /**
      * 回退任务
      * 
      * @param taskId
      */
     public void reject(Long taskId);
-    /**
-     * 回退任务
-     * 
-     * @param taskId
-     */
-    public void reject(Task task);    
     /**
      * 召回任务
      * 
@@ -50,11 +25,5 @@ public interface TaskService {
      * @param user
      */
     public void recall(Long taskId);
-    /**
-     * 召回任务
-     * 
-     * @param taskId
-     */
-    public void recall(Task task);
 
 }
