95355a8edbd57c3159e19129d721a5660ae7d779
Rui Tang <tangrui@zyeeda.com>
2012-12-24 17:10:56 +0800

    Synchronizing when check unique constraint. Fix #34

--- validation/src/main/java/com/zyeeda/framework/validation/validator/UniqueConstraintValidator.java
+++ validation/src/main/java/com/zyeeda/framework/validation/validator/UniqueConstraintValidator.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Parameter;
-import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 import javax.validation.ConstraintValidatorContext;
 
@@ -35,41 +34,38 @@ public class UniqueConstraintValidator extends CustomConstraintValidator<Unique,
 
 	@Override
 	public boolean isValid(Object obj, ConstraintValidatorContext ctx) {
-        try {
-            EntityManager session = EntityManagerFactoryUtils.doGetTransactionalEntityManager(this.getEntityManagerFactory(), null);
-            Query query = session.createNamedQuery(this.constraint.namedQuery());
-            Set<Parameter<?>> params = query.getParameters();
-            for (Parameter<?> param : params) {
+        EntityManager session = EntityManagerFactoryUtils.getTransactionalEntityManager(this.getEntityManagerFactory(), null);
+        Query query = session.createNamedQuery(this.constraint.namedQuery());
+        Set<Parameter<?>> params = query.getParameters();
+        for (Parameter<?> param : params) {
+            try {
+                String name = param.getName();
+                String replacedName = name.replace('$', '.');
+                Object value = null;
                 try {
-                    String name = param.getName();
-                    String replacedName = name.replace('$', '.');
-                    Object value = null;
-                    try {
-                        value = BeanUtils.getNestedProperty(obj, replacedName);
-                    } catch (NestedNullException e) {
-                        LOGGER.trace(e.getMessage(), e);
-                    }
-                    if (LOGGER.isDebugEnabled()) {
-                        LOGGER.debug("parameter name = {}", name);
-                        LOGGER.debug("replaced parameter name = {}", replacedName);
-                        LOGGER.debug("parameter value = {}", value);
-                    }
-                    
-                    query.setParameter(name, value);
-                } catch (NoSuchMethodException e) {
-                    LOGGER.error(e.getMessage(), e);
-                } catch (IllegalAccessException e) {
-                    LOGGER.error(e.getMessage(), e);
-                } catch (InvocationTargetException e) {
-                    LOGGER.error(e.getMessage(), e);
+                    value = BeanUtils.getNestedProperty(obj, replacedName);
+                } catch (NestedNullException e) {
+                    LOGGER.trace(e.getMessage(), e);
                 }
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("parameter name = {}", name);
+                    LOGGER.debug("replaced parameter name = {}", replacedName);
+                    LOGGER.debug("parameter value = {}", value);
+                }
+                
+                query.setParameter(name, value);
+            } catch (NoSuchMethodException e) {
+                LOGGER.error(e.getMessage(), e);
+            } catch (IllegalAccessException e) {
+                LOGGER.error(e.getMessage(), e);
+            } catch (InvocationTargetException e) {
+                LOGGER.error(e.getMessage(), e);
             }
+        }
+        synchronized(this) {
             long count = (Long) query.getSingleResult();
             return count == 0;
-        } catch (PersistenceException e) {
-            LOGGER.error(e.getMessage(), e);
         }
-        return false;
 	}
 
 }
