b80250be04edd8ca3b840b53a3756622c21f831c
陈海峰 <7720829@gmail.com>
2014-10-21 17:19:33 +0800

    modify test code

--- core/src/test/resources/tests/all.js
+++
@@ -1,6 +0,0 @@
-exports.testHello = require('hello/hello');
-exports.testMark  = require('mark/mark');
-
-if (require.main == module.id) {
-    require('test').run(exports);
-}
--- core/src/test/resources/tests/mark/test-mark.coffee
+++
@@ -1,5 +0,0 @@
-{mark} = require 'cdeio/mark'
-{ok} = require 'assert'
-
-exports.testMark = ->
-    ok(mark isnt null)
