a1bf13cc16c48ca8ce9905e27d910140661b7dbb
陈海峰 <7720829@gmail.com>
2014-10-21 17:20:07 +0800

    modify test code2

---
+++ core/src/test/javascript/mark/test-mark.coffee
@@ -0,0 +1,5 @@
+{mark} = require 'cdeio/mark'
+{ok} = require 'assert'
+
+exports.testMark = ->
+    ok(mark isnt null)
---
+++ testringo/src/test/javascript/file/all.js
@@ -0,0 +1,9 @@
+
+exports.testDirname = require('./dirname');
+exports.testExtension = require('./extension');
+exports.testIsAbsolute = require('./is-absolute');
+exports.testIterator = require('./iterator');
+exports.testNormal = require('./normal');
+exports.testRelative = require('./relative');
+exports.testResolve = require('./resolve');
+exports.testTouch = require("./touch");
