ef6cf892849e84de088c9088ab07a660c6cdf756
shupeipei <shupeipei@zyeeda.com>
2016-01-22 13:13:29 +0800

    为 app 提供访问库内部资源入口

---
+++ index.js
@@ -0,0 +1,7 @@
+import config from './lib/config'
+import logger from './lib/logger'
+
+import app from './lib/server'
+import models from './lib/server/models'
+
+export default {config, logger, app, models}
