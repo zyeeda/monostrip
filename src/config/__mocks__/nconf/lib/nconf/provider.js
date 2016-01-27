b17f891f14dcea960e419746dce9f1ba52f23eb1
Rui Tang <tangrui@zyeeda.com>
2016-01-27 19:10:06 +0800

    修改 .gitignore 使得 src/config/__mocks__/nconf/lib/nconf 里面的文件可以被加入到代码库.

---
+++ src/config/__mocks__/nconf/lib/nconf/provider.js
@@ -0,0 +1,17 @@
+const ProviderMock = jest.genMockFunction()
+
+const config = {}
+
+ProviderMock.prototype.argv = jest.genMockFunction().mockReturnThis()
+ProviderMock.prototype.env = jest.genMockFunction().mockReturnThis()
+ProviderMock.prototype.file = jest.genMockFunction().mockReturnThis()
+
+ProviderMock.prototype.set = jest.genMockFunction().mockImplementation((key, value) => {
+  config[key] = value
+})
+
+ProviderMock.prototype.get = jest.genMockFunction().mockImplementation((key) => {
+  return config[key]
+})
+
+export {ProviderMock as Provider}
