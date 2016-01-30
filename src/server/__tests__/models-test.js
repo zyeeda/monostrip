77620be258df2670343907b1e076a1c563ca0e84
Rui Tang <tangrui@zyeeda.com>
2016-01-30 18:38:12 +0800

    添加 models 的测试用例.

---
+++ src/server/__tests__/models-test.js
@@ -0,0 +1,63 @@
+import path from 'path'
+
+jest.dontMock('../models')
+
+// mock fs-plus
+const _files = ['account.js', 'group.js', 'project/task.js', 'project/phase.js']
+let _start = 0
+const fsMock = {
+  listTreeSync: jest.genMockFunction().mockImplementation((filePath) => {
+    return [`${filePath}/${_files[_start++]}`, `${filePath}/${_files[_start++]}`]
+  }),
+  isFileSync: jest.genMockFunction().mockReturnValue(true)
+}
+jest.setMock('fs-plus', fsMock)
+
+// mock sequelize
+const SequelizeMock = jest.genMockFunction()
+SequelizeMock.prototype.import = jest.genMockFunction().mockImplementation((filePath) => {
+  return {
+    name: path.basename(filePath, '.js'),
+    associate: jest.genMockFunction()
+  }
+})
+jest.setMock('sequelize', SequelizeMock)
+
+// test case
+describe('models', () => {
+  it('should import dedicated models', () => {
+    const fs = require('fs-plus')
+    const sequelize = require('sequelize')
+    const config = require('../../config').default
+
+    config.set('db:database', 'test-database')
+    config.set('db:username', 'test-user')
+    config.set('db:password', 'test-password')
+    config.set('db:options', {})
+    config.set('sysPath', '/path/to/sys')
+    config.set('appPath', '/path/to/app')
+
+    const {sequelize: sequelizeInstance, models} = require('../models')
+
+    expect(config.get.mock.calls.length).toBe(6)
+
+    expect(fs.listTreeSync.mock.calls.length).toBe(2)
+    expect(fs.listTreeSync.mock.calls[0][0]).toBe('/path/to/sys/lib/models')
+    expect(fs.listTreeSync.mock.calls[1][0]).toBe('/path/to/app/models')
+    expect(fs.isFileSync.mock.calls.length).toBe(4)
+    expect(fs.isFileSync.mock.calls[0][0]).toBe('/path/to/sys/lib/models/account.js')
+    expect(fs.isFileSync.mock.calls[1][0]).toBe('/path/to/sys/lib/models/group.js')
+    expect(fs.isFileSync.mock.calls[2][0]).toBe('/path/to/app/models/project/task.js')
+    expect(fs.isFileSync.mock.calls[3][0]).toBe('/path/to/app/models/project/phase.js')
+
+    expect(sequelize).toBeCalledWith('test-database', 'test-user', 'test-password', {})
+    console.log(sequelize.mock.instances[0])
+    expect(sequelize.mock.instances[0]).toBe(sequelizeInstance)
+    expect(sequelizeInstance.import.mock.calls.length).toBe(4)
+
+    const values = require('lodash.values')
+    values(models).forEach(model => {
+      expect(model.associate).toBeCalledWith(models)
+    })
+  })
+})
