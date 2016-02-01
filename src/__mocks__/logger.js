e2e65215f6ce42f375d745d196cbcf62e53f0be4
Rui Tang <tangrui@zyeeda.com>
2016-02-01 10:26:50 +0800

    添加缺省的 logger mock.

---
+++ src/__mocks__/logger.js
@@ -0,0 +1,8 @@
+export default {
+  fatal: jest.genMockFunction(),
+  error: jest.genMockFunction(),
+  warn: jest.genMockFunction(),
+  info: jest.genMockFunction(),
+  debug: jest.genMockFunction(),
+  trace: jest.genMockFunction()
+}
